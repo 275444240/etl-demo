ee6535e2d4afcbb0605c4af145a9cac3e879d0e3
Administrator <275444240@qq.com>
2020-04-19 12:17:33 +0800

    Revert "测试git回滚"
    This reverts commit a0bb03ec

--- src/main/java/com/ayuan/etldemo/EtlDemoApplication.java
+++ src/main/java/com/ayuan/etldemo/EtlDemoApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EtlDemoApplication {
-
+    /**
+     * 测试
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(EtlDemoApplication.class, args);
     }
