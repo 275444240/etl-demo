a0bb03ec13f49bf9fccca7838e55ac758ea1d9b7
Administrator <275444240@qq.com>
2020-04-19 12:13:22 +0800

    测试git回滚

--- src/main/java/com/ayuan/etldemo/EtlDemoApplication.java
+++ src/main/java/com/ayuan/etldemo/EtlDemoApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EtlDemoApplication {
-
+    /**
+     * 测测git 回滚
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(EtlDemoApplication.class, args);
     }
